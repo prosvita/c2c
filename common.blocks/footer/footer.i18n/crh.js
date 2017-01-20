69bc8d0f29ed0cad543da1b00a187b4fe4d65da5
Pavel Bashinsky <pbashinsky@yandex-team.ru>
2017-01-20 19:02:06 +0300

    Переклад

--- common.blocks/footer/footer.i18n/crh.js
+++ common.blocks/footer/footer.i18n/crh.js
@@ -1,12 +1,12 @@
 module.exports = {
     footer: {
-        'about-title': "Про нас",
-        'about': "",
-        'feedback-title': "",
-        'feedback': "",
-        'bugreport': "",
-        'disclaimers-title': "Відмова від відповідальності",
-        'disclaimers': "",
-        'organisation': "“Просвіта”"
+        'about-title': 'Biz aqqında',
+        'about': '“Просвіта” — Ukraina halqlarnıñ milliy medeniyet ve özgünlini saqlamaq maqsadınen bağlı adiy insanlar. Biz qayd etilmegen teşkilâtmız ve böyle ya da oşağan adlarnı taşığan teşkilâtlarnen bağlı degilmiz.',
+        'feedback-title': 'Arqa bağ',
+        'feedback': 'Bir-de bir malümatlarnı bilmek içün,bizge mesac yaza bilesiñiz. Biz mutlaqa sizge cevap yazmağa tırışarıq. Eger malümat faydalı olsa, biz onı saytımızda yerleştirerik.',
+        'bugreport': 'Yeger sizge qata tapqan olsañız ya da malümat qoşmaq isteseñiz, bizge yaza bilesiñiz.',
+        'disclaimers-title': 'Mesülietten red etüv',
+        'disclaimers': 'Sayt berilgen malümatnıñ aynılığı içün özüni mesüliyetlikten boşata. Daa doğru malümatlarnı bilmek içün berilgen bağ operatorlarına muraacatta buluna bilesiñiz.',
+        'organisation': '“Просвіта”'
     }
 };
